7e107536c1fbbf4588049131e12beb87b96392ca
Radwa Khattab <RadwaSM@hotmail.com>
2019-12-13 01:24:05 +0200

    hough transform lines
    Works pretty well till detecting both horizontal and vertical lines, and sorts them into two different sets.
    The code still doesn't run efficiently on all images, but its results is good on the image used in the function call.

---
+++ module1/module1.py
@@ -0,0 +1,154 @@
+import numpy as np
+import cv2 as cv
+import math
+import os
+
+
+def readImages(directory):
+    fnames = os.listdir(directory)
+    to_return = []
+    for fn in fnames:
+        path = os.path.join(directory, fn)
+        gray_scale_image = cv.cvtColor(cv.imread(path), cv.COLOR_BGR2GRAY)
+        to_return.append((fn, gray_scale_image))
+
+    return to_return
+
+def remove_duplicates(lines):
+    for i, (rho, theta) in enumerate(lines):
+        for j, (rho2, theta2) in enumerate(lines):
+            if j == i:
+                continue
+            deltaRho = abs(abs(rho) - abs(rho2))
+            deltaTheta = abs(abs(theta) - abs(theta2))
+            if deltaRho < 20 and deltaTheta < 35.0/180.0:
+                del lines[j]
+    return lines
+
+def sort_line_list(lines):
+    vertical = []
+    horizontal = []
+    for rho, theta in lines:
+        if theta > 1 and theta < 2:
+            horizontal.append((rho, theta))
+        else:
+            vertical.append((rho, theta))
+    return horizontal, vertical
+
+# def hough_transform_p(image, template, tableCnt):
+#     # open and process images
+#     img = cv2.imread('imgs/'+image)
+#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+#
+#     # probabilistic hough transform
+#     lines = cv2.HoughLinesP(edges, 1, np.pi/180, 200, minLineLength=20, maxLineGap=999)[0].tolist()
+#
+#     # remove duplicates
+#     lines = remove_duplicates(lines)
+#
+#     # draw image
+#     for x1, y1, x2, y2 in lines:
+#         cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
+#
+#     # sort lines into vertical & horizontal lists
+#     horizontal, vertical = sort_line_list(lines)
+#
+#     # go through each horizontal line (aka row)
+#     rows = []
+#     for i, h in enumerate(horizontal):
+#         if i < len(horizontal)-1:
+#             row = []
+#             for j, v in enumerate(vertical):
+#                 if i < len(horizontal)-1 and j < len(vertical)-1:
+#                     # every cell before last cell
+#                     # get width & height
+#                     width = horizontal[i+1][1] - h[1]
+#                     height = vertical[j+1][0] - v[0]
+#
+#                 else:
+#                     # last cell, width = cell start to end of image
+#                     # get width & height
+#                     width = tW
+#                     height = tH
+#                 tW = width
+#                 tH = height
+#
+#                 # get roi (region of interest) to find an x
+#                 roi = img[h[1]:h[1]+width, v[0]:v[0]+height]
+#
+#                 # save image (for testing)
+#                 dir = 'imgs/table%s' % (tableCnt+1)
+#                 if not os.path.exists(dir):
+#                     os.makedirs(dir)
+#                 fn = '%s/roi_r%s-c%s.png' % (dir, i, j)
+#                 cv2.imwrite(fn, roi)
+#
+#                 # if roi contains an x, add x to array, else add _
+#                 roi_gry = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+#                 ret, thresh = cv2.threshold(roi_gry, 127, 255, 0)
+#                 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#
+#                 if len(contours) > 1:
+#                     # there is an x for 2 or more contours
+#                     row.append('x')
+#                 else:
+#                     # there is no x when len(contours) is <= 1
+#                     row.append('_')
+#             row.pop()
+#             rows.append(row)
+#
+#     # save image (for testing)
+#     fn = os.path.splitext(image)[0] + '-hough_p.png'
+#     cv2.imwrite('imgs/'+fn, img)
+#
+#
+# def process(images):
+#     for i, img in enumerate(images):
+#         # perform probabilistic hough transform on each image
+#         hough_transform_p(img, templates[0], i)
+
+def FixList(list):
+    newList = []
+    for item in list:
+        newList.append(item[0])
+    return newList
+
+def Closing(img):
+    SE = np.ones((3,3), np.uint8)
+    ret, img = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+    img = cv.dilate(img, SE)
+    img = cv.erode(img, SE)
+    return img
+
+def HoughLinesManual(pic):
+    img = pic.copy()
+    canny_img = cv.Canny(img, 100, 350)
+
+    linesTemp = cv.HoughLines(canny_img, 1, np.pi / 180.0, 280)
+    lines = FixList(linesTemp)
+    print(len(lines))
+
+    remove_duplicates(lines)
+    print(len(lines))
+
+    hLines, vLines = sort_line_list(lines)
+
+    if lines is not None:
+        for rho, theta in lines:
+            print(rho, theta)
+            a = math.cos(theta)
+            b = math.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+            pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+            cv.line(img, pt1, pt2, (0, 0, 255), 3, cv.LINE_AA)
+
+    cv.imshow("Detected Lines", img)
+
+
+# __main__
+dataset = readImages(r'dataset_module1')
+HoughLinesManual(dataset[2][1])
+cv.waitKey()
